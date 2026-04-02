e6e3a57390d8bfda443f28123e7e1a6c34639077
Himanshu Singh <himanshu124singh19@gmail.com>
2026-04-02 21:58:01 +0530

    Added Dependencies and Created Pipeline

---
+++ src/main/java/com/nlp/NLP/Pipeline.java
@@ -0,0 +1,25 @@
+package com.nlp.NLP;
+
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+
+import java.util.Properties;
+
+public class Pipeline {
+    private static Properties properties;
+    private static String propertiesName = "";
+    private static StanfordCoreNLP stanfordCoreNLP;
+
+    private Pipeline() {}
+
+    static {
+        properties = new Properties();
+        properties.setProperty("annotators",propertiesName);
+    }
+
+    private static StanfordCoreNLP getStanfordCoreNLP() {
+        if (stanfordCoreNLP == null) {
+            stanfordCoreNLP = new StanfordCoreNLP(properties);
+        }
+        return stanfordCoreNLP;
+    }
+}
